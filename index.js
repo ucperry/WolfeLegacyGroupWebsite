39a9e67169a79e29ee0c53ee243d8961c6b66f66
ucperry <p.wolfe@icloud.com>
2024-03-19 17:55:40 -0400

    all lessaway Timeouts (home and away) are buttons are functioning

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let homeTimeoutsLeftEl = document.getElementById("homeTimeoutsLeft-el")
+let awayTimeoutsLeftEl = document.getElementById("awayTimeoutsLeft-el")
 
 let homeTOL = 5
+let awayTOL = 5
 
 function lessHomeTimeouts() {
     homeTOL = homeTOL - 1
     homeTimeoutsLeftEl.innerText = homeTOL
 }
 
+function lessAwayTimeouts() {
+    awayTOL = awayTOL - 1
+    awayTimeoutsLeftEl.innerText = awayTOL
+}
