c361132f7b5ad4cc4c9e59bfbdb23bf9999d6087
ucperry <p.wolfe@icloud.com>
2024-03-19 17:48:00 -0400

    home Timeouts Left decrease function works

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let homeTimeoutsLeftEl = document.getElementById("homeTimeoutsLeft-el")
+
+let homeTOL = 5
+
+function lessHomeTimeouts() {
+    homeTOL = homeTOL - 1
+    homeTimeoutsLeftEl.innerText = homeTOL
+}
 
