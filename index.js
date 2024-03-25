146190f62dd45ddfdea5ba930b8146b58f7f39cd
ucperry <p.wolfe@icloud.com>
2024-03-25 14:57:08 -0400

    put all JS in idex js clock is working but now layout is stretched

--- index.js
+++ index.js
@@ -1,25 +1,115 @@
 // Game Information Functions
-import Timer from "./timer"
-
-new Timer(
-    document.querySelector(".timer")
-)
-
-let homeTOL = 5
-let homeTimeoutsLeftEl = document.getElementById("homeTimeoutsLeft-el")
-    function lessHomeTimeouts() {
-    if (homeTOL > 0) {
-        homeTOL = homeTOL - 1} else {homeTOL=0}
-        homeTimeoutsLeftEl.innerText = homeTOL
+    class Timer {
+        constructor(root) {
+        root.innerHTML = Timer.getHTML();
+    
+        this.el = {
+            minutes: root.querySelector(".timer__part--minutes"),
+            seconds: root.querySelector(".timer__part--seconds"),
+            control: root.querySelector(".timer__btn--control"),
+            reset: root.querySelector(".timer__btn--reset")
+        };
+    
+        this.interval = null;
+        this.remainingSeconds = 0;
+    
+        this.el.control.addEventListener("click", () => {
+            if (this.interval === null) {
+            this.start();
+            } else {
+            this.stop();
+            }
+        });
+    
+        this.el.reset.addEventListener("click", () => {
+            const inputMinutes = prompt("Enter number of minutes:");
+    
+            if (inputMinutes < 60) {
+            this.stop();
+            this.remainingSeconds = inputMinutes * 60;
+            this.updateInterfaceTime();
+            }
+        });
+        }
+    
+        updateInterfaceTime() {
+        const minutes = Math.floor(this.remainingSeconds / 60);
+        const seconds = this.remainingSeconds % 60;
+    
+        this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+        this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+        }
+    
+        updateInterfaceControls() {
+        if (this.interval === null) {
+            this.el.control.innerHTML = `<span class="material-symbols-outlined">play_arrow</span>`;
+            this.el.control.classList.add("timer__btn--start");
+            this.el.control.classList.remove("timer__btn--stop");
+        } else {
+            this.el.control.innerHTML = `<span class="material-symbols-outlined">pause</span>`;
+            this.el.control.classList.add("timer__btn--stop");
+            this.el.control.classList.remove("timer__btn--start");
+        }
+        }
+    
+        start() {
+        if (this.remainingSeconds === 0) return;
+    
+        this.interval = setInterval(() => {
+            this.remainingSeconds--;
+            this.updateInterfaceTime();
+    
+            if (this.remainingSeconds === 0) {
+            this.stop();
+            }
+        }, 1000);
+    
+        this.updateInterfaceControls();
+        }
+    
+        stop() {
+        clearInterval(this.interval);
+    
+        this.interval = null;
+    
+        this.updateInterfaceControls();
+        }
+    
+        static getHTML() {
+        return `
+            <span class="timer__part timer__part--minutes">00</span>
+            <span class="timer__part">:</span>
+            <span class="timer__part timer__part--seconds">00</span>
+            <button type="button" class="timer__btn timer__btn--control timer__btn--start">
+            <span class="material-symbols-outlined">play_arrow</span>
+            </button>
+            <button type="button" class="timer__btn timer__btn--reset">
+            <span class="material-symbols-outlined">timer</span>
+            </button>
+            `;
+        }
     }
+    
+    new Timer(
+        document.querySelector(".timer")
+    );
 
-let awayTOL = 5    
-let awayTimeoutsLeftEl = document.getElementById("awayTimeoutsLeft-el")
-    function lessAwayTimeouts() {
-    if (awayTOL > 0) {
-        awayTOL = awayTOL - 1} else {awayTOL=0}
-        awayTimeoutsLeftEl.innerText = awayTOL
-    }
+    // Timeouts Left functions    
+    let homeTOL = 5
+    let homeTimeoutsLeftEl = document.getElementById("homeTimeoutsLeft-el")
+        function lessHomeTimeouts() {
+        if (homeTOL > 0) {
+            homeTOL = homeTOL - 1} else {homeTOL=0}
+            homeTimeoutsLeftEl.innerText = homeTOL
+        }
+
+    let awayTOL = 5    
+    let awayTimeoutsLeftEl = document.getElementById("awayTimeoutsLeft-el")
+        function lessAwayTimeouts() {
+        if (awayTOL > 0) {
+            awayTOL = awayTOL - 1} else {awayTOL=0}
+            awayTimeoutsLeftEl.innerText = awayTOL
+        }
 
 // HOME TEAM 
     let totalHomeTeamGamePoints = 0
